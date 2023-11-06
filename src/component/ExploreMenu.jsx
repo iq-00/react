27b78b69d0c8cbdc3c3ad52051d680620f343e51
sub_has <102864350+iq-00@users.noreply.github.com>
2023-11-06 21:10:56 +0530

    Add files via upload

---
+++ src/component/ExploreMenu.jsx
@@ -0,0 +1,76 @@
+export default function ExploreMenu() {
+  return (
+    <>
+      <div id="exploreMenu">
+        <h1>Explore Menu</h1>
+
+        <div className="explore-cards">
+          <div className="explore-card">
+            <img
+              src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/em-ginger-fried-img.png"
+              alt=""
+            />
+            <h4>Non-Veg Starters</h4>
+            <a href="/">View All &#8594;</a>
+          </div>
+          <div className="explore-card">
+            <img
+              src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/em-ginger-fried-img.png"
+              alt=""
+            />
+            <h4>Non-Veg Starters</h4>
+            <a href="/">View All &#8594;</a>
+          </div>
+          <div className="explore-card">
+            <img
+              src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/em-ginger-fried-img.png"
+              alt=""
+            />
+            <h4>Non-Veg Starters</h4>
+            <a href="/">View All &#8594;</a>
+          </div>
+          <div className="explore-card">
+            <img
+              src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/em-ginger-fried-img.png"
+              alt=""
+            />
+            <h4>Non-Veg Starters</h4>
+            <a href="/">View All &#8594;</a>
+          </div>
+          <div className="explore-card">
+            <img
+              src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/em-ginger-fried-img.png"
+              alt=""
+            />
+            <h4>Non-Veg Starters</h4>
+            <a href="/">View All &#8594;</a>
+          </div>
+          <div className="explore-card">
+            <img
+              src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/em-ginger-fried-img.png"
+              alt=""
+            />
+            <h4>Non-Veg Starters</h4>
+            <a href="/">View All &#8594;</a>
+          </div>
+          <div className="explore-card">
+            <img
+              src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/em-ginger-fried-img.png"
+              alt=""
+            />
+            <h4>Non-Veg Starters</h4>
+            <a href="/">View All &#8594;</a>
+          </div>
+          <div className="explore-card">
+            <img
+              src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/em-ginger-fried-img.png"
+              alt=""
+            />
+            <h4>Non-Veg Starters</h4>
+            <a href="/">View All &#8594;</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
